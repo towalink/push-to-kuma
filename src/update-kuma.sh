f3b60ebae82d0733459000a15f43cc05fb8ec891
Henri <git@henrici.name>
2026-02-05 12:54:15 +0100

    Refactor API for considering more generic functionality

--- src/update-kuma.sh
+++ src/update-kuma.sh
@@ -8,13 +8,13 @@
 # License:     MIT
 
 
-# Posts the disk usage to the provided URL
-function postDiskUsageGeneric #(disk, threshold, push_url, type)
+# Send a status message to the provided Kuma URL
+function postToKuma #(push_url, type, disk, threshold)
 {
-    disk="$1"
-    threshold="$2"
-    push_url="$3"
-    type="$4"
+    push_url="$1"
+    type="$2"
+    disk="$3"
+    threshold="$4"
     percentage="0%"
     health=""
     message=""
@@ -71,23 +71,23 @@ function postDiskUsageGeneric #(disk, threshold, push_url, type)
 
 
 # Posts the disk usage to the provided URL
-function postDiskUsage #(disk, threshold, push_url)
+function postDiskUsage #(push_url, disk, threshold)
 {
-    postDiskUsageGeneric "$1" "$2" "$3" disk
+    postToKuma "$1" disk "$2" "$3"
 }
 
 
 # Posts the disk usage to the provided URL
-function postMdraidStatus #(device, push_url)
+function postMdraidStatus #(push_url, device)
 {
-    postDiskUsageGeneric "$1" 0 "$2" mdraid
+    postToKuma "$1" mdraid "$2" 0
 }
 
 
 # Posts the disk usage to the provided URL
-function postDiskUsageZFS #(pool, threshold, push_url)
+function postDiskUsageZFS #(push_url, pool, threshold)
 {
-    postDiskUsageGeneric "$1" "$2" "$3" ZFS
+    postToKuma "$1" ZFS "$2" "$3"
 }
 
 
@@ -95,12 +95,12 @@ function postDiskUsageZFS #(pool, threshold, push_url)
 
 # Example:
 # For disks listed with the "df" command; considered down if disk usage exceeds the given threshold (here: 80%)
-postDiskUsage "/home" 80 "https://kuma.mydomain.com/api/push/1234567890"
+postDiskUsage "https://kuma.mydomain.com/api/push/1234567890" "/home" 80
 
 # Example:
 # For mdraid device listed with the "mdadm" command; considered down if not in "active" or "clean" state
-postMdraidStatus "/dev/md0" "https://kuma.mydomain.com/api/push/1234567890"
+postMdraidStatus "https://kuma.mydomain.com/api/push/1234567890" "/dev/md0"
 
 # Example:
 # For ZFS pools listed with the "zpool list" command; considered down if disk usage exceeds the given threshold (here: 65%) or not in "ONLINE" state
-postDiskUsageZFS "tank" 65 "https://kuma.mydomain.com/api/push/1234567890"
+postDiskUsageZFS "https://kuma.mydomain.com/api/push/1234567890" "tank" 65
