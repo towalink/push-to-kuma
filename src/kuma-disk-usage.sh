56288363111ae31e2f4e24ad69022547784802cf
Henri <git@henrici.name>
2026-02-05 12:26:24 +0100

    Add mdraid status query

--- src/kuma-disk-usage.sh
+++ src/kuma-disk-usage.sh
@@ -15,19 +15,37 @@ function postDiskUsageGeneric #(disk, threshold, push_url, type)
     threshold="$2"
     push_url="$3"
     type="$4"
+    percentage="0%"
     health=""
+    message=""
     service_status="up"
 
     case ${type} in
         disk)
             percentage=$(df -hP "${disk}" | tail -1 | awk '{printf "%s", $5}')
+            if [[ -z "${percentage}" ]]; then
+                echo "Error querying disk usage"
+                exit 127
+            fi
+            # The following text may be changed if desired
+            message="Disk usage on ${disk} is ${percentage}"
+            ;;
+        mdraid)
+            health=$(/usr/sbin/mdadm --detail "${disk}" | grep -F "State :" | awk '{printf "%s", $3}')
+            if [[ "${health}" != "active" ]] && [[ "${health}" != "clean" ]]; then
+                service_status="down"
+            fi
+            # The following text may be changed if desired
+            message="${disk} is ${health}"
             ;;
         ZFS)
-            percentage=$(zpool list "${disk}" | tail -1 | awk '{printf "%s", $8}')
-            health=$(zpool list "${disk}" | tail -1 | awk '{printf "%s", $10}')
+            percentage=$(/usr/sbin/zpool list "${disk}" | tail -1 | awk '{printf "%s", $8}')
+            health=$(/usr/sbin/zpool list "${disk}" | tail -1 | awk '{printf "%s", $10}')
             if [[ "${health}" != "ONLINE" ]]; then
                 service_status="down"
             fi
+            # The following text may be changed if desired
+            message="Disk usage on ${disk} is ${percentage}, status ${health}"
             ;;
         *)
             echo "Type not recognized."
@@ -36,18 +54,8 @@ function postDiskUsageGeneric #(disk, threshold, push_url, type)
     esac
 
     number=${percentage%\%*}
-    if [[ -z "${number}" ]]; then
-        echo "Error querying disk usage"
-        exit 127
-    fi
-    if [[ ${number} -ge ${threshold} ]]; then
+    if [[ ${number} -gt ${threshold} ]]; then
         service_status="down"
-    fi
-
-    # The following text may be changed if desired
-    message="Disk usage on ${disk} is ${number}%"
-    if [[ -n "${health}" ]]; then
-        message+=", status ${health}"
     fi
 
     echo "Posting status '${service_status}' with message '${message}'"
@@ -70,6 +78,13 @@ function postDiskUsage #(disk, threshold, push_url)
 
 
 # Posts the disk usage to the provided URL
+function postMdraidStatus #(device, push_url)
+{
+    postDiskUsageGeneric "$1" 0 "$2" mdraid
+}
+
+
+# Posts the disk usage to the provided URL
 function postDiskUsageZFS #(pool, threshold, push_url)
 {
     postDiskUsageGeneric "$1" "$2" "$3" ZFS
@@ -81,6 +96,10 @@ function postDiskUsageZFS #(pool, threshold, push_url)
 # Example:
 # For disks listed with the "df" command; considered down if disk usage exceeds the given threshold (here: 80%)
 postDiskUsage "/home" 80 "https://kuma.mydomain.com/api/push/1234567890"
+
+# Example:
+# For mdraid device listed with the "mdadm" command; considered down if not in "active" or "clean" state
+postMdraidStatus "/dev/md0" "https://kuma.mydomain.com/api/push/1234567890"
 
 # Example:
 # For ZFS pools listed with the "zpool list" command; considered down if disk usage exceeds the given threshold (here: 65%) or not in "ONLINE" state
